1792182bd5b8250567d41458ab26b9d6b9ce8a7e
Lumin228 <6786679@gmail.com>
2023-11-14 00:11:25 +0200

    1-st part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulList = document.querySelector('.gallery');
+
+function createLi(galleryItems) {
+    for (let i = 0; i < galleryItems.length; i++ ) {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const ahr = document.createElement('a')
+        img.src = galleryItems[i].preview;
+        img.alt = galleryItems[i].description;
+        img.dataset.source = galleryItems[i].original;
+        ahr.href = galleryItems[i].original;
+        img.classList.add('gallery__image');
+        ahr.classList.add('gallery__link');
+        li.classList.add('gallery__item');
+       
+        li.appendChild(img); 
+        li.append(ahr);
+        ulList.appendChild(li);  
+    }
+}
+createLi(galleryItems)
+const liCheck = document.querySelector('.gallery__item');
+console.log(liCheck)
